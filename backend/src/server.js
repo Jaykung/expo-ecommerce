0196d810101c6ec4ae9ba91784f88b176cc7a4a3
Thapanawat Pongpattarawat <emilio089@gmail.com>
2026-07-21 12:46:42 +0700

    Added db config & tools

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
+import { clerkMiddleware } from "@clerk/express";
 import express from "express";
 import path from "path";
+import { connectDB } from "./config/db.js";
 import { ENV } from "./config/env.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+app.use(clerkMiddleware());
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
@@ -20,4 +24,7 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-app.listen(ENV.PORT, () => console.log("Server is up and running"));
+app.listen(ENV.PORT, () => {
+  console.log("Server is up and running");
+  connectDB();
+});
