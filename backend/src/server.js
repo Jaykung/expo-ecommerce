1975abe481da10650d2bef14b3ccbecde8d5415a
Thapanawat Pongpattarawat <emilio089@gmail.com>
2026-07-28 21:41:16 +0700

    Added user routes and controller

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import { ENV } from "./config/env.js";
 import { functions, inngest } from "./config/inngest.js";
 
 import adminRoutes from "./routes/admin.route.js";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(clerkMiddleware());
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api/admin", adminRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
