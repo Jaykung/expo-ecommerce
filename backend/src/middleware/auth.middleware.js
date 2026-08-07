b1f9824b50d415c4331dee38168da13dcaa7a987
Thapanawat Pongpattarawat <emilio089@gmail.com>
2026-08-07 19:26:30 +0700

    Bug: Added missing "next" parameter in auth.middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -23,7 +23,7 @@ export const protectRoute = [
   }
 ];
 
-export const adminOnly = (req, res) => {
+export const adminOnly = (req, res, next) => {
   if (!req.user) {
     return res.status(401).json({ message: "Unauthorized - user not found" });
   }
