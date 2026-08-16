1c6abb858460f5bb3c3bfc2ec2a51f6a09613b77
Thapanawat Pongpattarawat <emilio089@gmail.com>
2026-08-16 15:03:07 +0700

    Added customers page & sentry.io

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/react";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router";
+import * as Sentry from "@sentry/react";
 
 import {
   QueryClient,
@@ -19,6 +20,22 @@ if (!PUBLISHABLE_KEY) {
 }
 
 const queryClient = new QueryClient();
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: []
+  },
+  integrations: [
+    Sentry.replayIntegration()
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
