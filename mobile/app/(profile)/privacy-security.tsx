53d090028bf295ab70abaaf26773defb9e40f493
Thapanawat Pongpattarawat <emilio089@gmail.com>
2026-08-27 10:14:44 +0700

    Added profile tab page

---
+++ mobile/app/(profile)/privacy-security.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const PrivacyAndSecurityScreen = () => {
+  return (
+    <View>
+      <Text>PrivacyAndSecurityScreen</Text>
+    </View>
+  )
+}
+
+export default PrivacyAndSecurityScreen
